ea497d6a5bc0463ef814c7a8612f7b02b0318200
apple <chenjinkun_429@sjtu.edu.cn>
2020-04-09 18:40:20 +0800

    add test for PAPI_MLPX

--- papi_multiplex_test.c
+++ papi_multiplex_test.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 //global values for counters
 long long *counter_values;
@@ -11,15 +12,19 @@ char **testEventname;
 int eventSet;
 int NUM_EVENTS;
 
+PAPI_MLPX_attr attr;
 FILE *fout;
+struct timespec start, end;
 
 int overflow_cycle[]={1000000, 1000000, 130000}; //for cascade, set T_{mlpx} = T_{papisample}
 
 //helperfunctions
 void result_to_file(FILE *fp, long long *counter_values)
 {
+    int i;
+    
     fprintf(fp, "%lld", counter_values[1]); 
-    for(int i = 2; i < NUM_EVENTS+1; ++i)
+    for(i = 2; i < NUM_EVENTS+1; ++i)
     {
         fprintf(fp, ",%lld", counter_values[i]);
     }
@@ -35,6 +40,8 @@ void sampling_handler(int eventSet, void *address, long_long overflow_vector, vo
 //lib functions
 void mytest_papi_init(char *filename, char *kernelname, int mode, int arch)
 {
+    int i, id;
+    
     NUM_EVENTS = 0;
     PAPI_library_init(PAPI_VER_CURRENT);
     FILE *fevent = fopen(filename, "r");
@@ -54,7 +61,7 @@ void mytest_papi_init(char *filename, char *kernelname, int mode, int arch)
 
     fevent = fopen(filename, "r");
 
-    for(int i = 0; i < NUM_EVENTS; ++i)
+    for(i = 0; i < NUM_EVENTS; ++i)
 	{
         testEventname[i]=(char*)malloc(1024*sizeof(char));
         fscanf(fevent, "%s\n", testEventname[i]);
@@ -81,7 +88,7 @@ void mytest_papi_init(char *filename, char *kernelname, int mode, int arch)
     }
 
     PAPI_add_event(eventSet, PAPI_TOT_CYC);
-    for(int id = 0; id < NUM_EVENTS; ++id)
+    for(id = 0; id < NUM_EVENTS; ++id)
     {
         int retval = PAPI_add_event(eventSet, testEvent[id]);
         if(retval != PAPI_OK)
@@ -105,7 +112,7 @@ void mytest_papi_init(char *filename, char *kernelname, int mode, int arch)
 
     fout = fopen(outfilename, "w");
     fprintf(fout, "%s", testEventname[0]);
-    for(int i = 1; i < NUM_EVENTS; ++i)
+    for(i = 1; i < NUM_EVENTS; ++i)
     {
         fprintf(fout, ",%s", testEventname[i]);
     }
@@ -116,13 +123,120 @@ void mytest_papi_init(char *filename, char *kernelname, int mode, int arch)
 
 void mytest_papi_stop()
 {
+    int i;
+
     PAPI_stop(eventSet, counter_values);
     result_to_file(fout, counter_values);
     fclose(fout);
 
     free(testEvent);
     free(counter_values);
-    for(int i = 0; i < NUM_EVENTS; ++i)
+    for(i = 0; i < NUM_EVENTS; ++i)
+	{
+        free(testEventname[i]);
+	}
+    free(testEventname);
+}
+
+//lib functions for counting
+//0 for multiplex, -1 for accurate multiplex, others for ocoe
+void mytest_papi_count_init(char *filename, char *kernelname, int mode, int arch)
+{
+    int i, id;
+    
+    NUM_EVENTS = 0;
+    PAPI_library_init(PAPI_VER_CURRENT);
+    FILE *fevent = fopen(filename, "r");
+
+    char buf[1024];
+    while(fgets(buf, 1024, fevent))
+    {
+        NUM_EVENTS++;
+    }
+    printf("num_events = %d\n", NUM_EVENTS);
+    fclose(fevent);
+
+    //allocate local array
+    testEvent = (int*)malloc(NUM_EVENTS*sizeof(int));
+    counter_values = (long long*)malloc((NUM_EVENTS)*sizeof(long long));
+    testEventname = (char**)malloc(NUM_EVENTS*sizeof(char*));
+
+    fevent = fopen(filename, "r");
+
+    for(i = 0; i < NUM_EVENTS; ++i)
+	{
+        testEventname[i]=(char*)malloc(1024*sizeof(char));
+        fscanf(fevent, "%s\n", testEventname[i]);
+		PAPI_event_name_to_code(testEventname[i], &testEvent[i]);
+	}
+    fclose(fevent);
+
+    eventSet = PAPI_NULL;
+
+    if(mode == -1)
+    {
+	    memset(&attr, 0x0, sizeof(PAPI_MLPX_attr));
+        PAPI_MLPX_sampling_init(&attr);
+        PAPI_MLPX_create_eventset(&attr, &eventSet);
+        for(id = 0; id < NUM_EVENTS; ++id)
+        {
+            PAPI_MLPX_add_event(&attr, eventSet, testEvent[id]);
+        }
+        PAPI_MLPX_start(&attr, eventSet, 100000000);
+    }
+    else
+    {
+        PAPI_create_eventset(&eventSet);
+        PAPI_assign_eventset_component(eventSet, 0);
+
+        if(mode == 0) //multiplex
+        {
+            PAPI_set_multiplex(eventSet);
+        }
+
+        for(id = 0; id < NUM_EVENTS; ++id)
+        {
+            int retval = PAPI_add_event(eventSet, testEvent[id]);
+            if(retval != PAPI_OK)
+            {
+                printf("Failed to add %s\n", testEventname[id]);
+                exit(-1);
+            }
+        }
+
+        PAPI_start(eventSet);
+    }
+
+    // clock_gettime(CLOCK_MONOTONIC, &start);
+}
+
+void mytest_papi_count_stop(int mode)
+{
+    int i;
+
+    if(mode == -1)
+    {
+        PAPI_MLPX_stop(&attr, eventSet, counter_values, 0);
+    }
+    else
+    {
+        PAPI_stop(eventSet, counter_values);
+    }
+
+    // clock_gettime(CLOCK_MONOTONIC, &end);
+    // double time_elapsed = end.tv_sec - start.tv_sec + (end.tv_nsec - start.tv_nsec) * 1e-9;
+
+    //display the result
+    fprintf(stderr, "PAPI result in mode %d:\n", mode);
+    for(i = 0; i < NUM_EVENTS; ++i)
+    {
+        fprintf(stderr,"\t%s: %lld\n", testEventname[i], counter_values[i]);
+    }
+    // fprintf(stderr, "Total time elapsed with profile: %lf s\n", time_elapsed);
+
+    free(testEvent);
+    free(counter_values);
+    for(i = 0; i < NUM_EVENTS; ++i)
 	{
         free(testEventname[i]);
 	}
--- papi_multiplex_test.h
+++ papi_multiplex_test.h
@@ -2,6 +2,7 @@
 #define PAPI_MPX_TEST_H
 
 #include "papi.h"
+#include "papi_accurate_multiplexing.h"
 
 //lib functions api
 #ifdef __cplusplus
@@ -10,6 +11,10 @@ extern "C"{
 void mytest_papi_init(char *filename, char *kernelname, int mpx, int arch);
 
 void mytest_papi_stop();
+
+void mytest_papi_count_init(char *filename, char *kernelname, int mode, int arch);
+
+void mytest_papi_count_stop(int mode);
 
 #ifdef __cplusplus
 }
